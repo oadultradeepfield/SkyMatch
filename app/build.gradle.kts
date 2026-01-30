c5d7f840b4f394917e931f08aef274906b3eaf06
phanuphat <phanuphat@bytedance.com>
2026-01-30 14:40:34 +0800

    build: Setup build config for DI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,47 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.hilt.android)
-    alias(libs.plugins.ksp)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.hilt.android)
+  alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "com.oadultradeepfield.skymatch"
-    compileSdk {
-        version = release(36)
-    }
+  namespace = "com.oadultradeepfield.skymatch"
+  compileSdk { version = release(36) }
 
-    defaultConfig {
-        applicationId = "com.oadultradeepfield.skymatch"
-        minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "com.oadultradeepfield.skymatch"
+    minSdk = 26
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildFeatures { buildConfig = true }
+
+  buildTypes {
+    debug { buildConfigField("Boolean", "IS_DEBUG", "true") }
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      buildConfigField("Boolean", "IS_DEBUG", "false")
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.material)
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.lifecycle.viewmodel.compose)
+  implementation(libs.material)
+  implementation(libs.hilt.android)
+  ksp(libs.hilt.compiler)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
 }
