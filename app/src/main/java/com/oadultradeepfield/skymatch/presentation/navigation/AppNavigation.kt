026cbd45ddba1314eab2f961eeebe3b8e583e54c
phanuphat <phanuphat@bytedance.com>
2026-02-05 12:26:27 +0800

    refactor: Migrate AppNavigation to type-safe navigation

--- app/src/main/java/com/oadultradeepfield/skymatch/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/oadultradeepfield/skymatch/presentation/navigation/AppNavigation.kt
@@ -1,47 +1,47 @@
 package com.oadultradeepfield.skymatch.presentation.navigation
 
-import android.net.Uri
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.oadultradeepfield.skymatch.presentation.home.HomeScreen
 import com.oadultradeepfield.skymatch.presentation.search.SearchScreen
 import com.oadultradeepfield.skymatch.presentation.solving.SolvingScreen
 
+/** Main navigation host using type-safe routes. */
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
-  val navController = rememberNavController()
+    val navController = rememberNavController()
 
-  NavHost(
-      navController = navController,
-      startDestination = "home",
-      modifier = modifier.fillMaxSize(),
-  ) {
-    composable("home") {
-      HomeScreen(
-          onNavigateToUpload = { imageUris ->
-            val encodedUris = imageUris.joinToString(",") { Uri.encode(it) }
-            navController.navigate("solving/$encodedUris")
-          },
-          onNavigateToSearch = { navController.navigate("search") },
-      )
+    NavHost(
+        navController = navController,
+        startDestination = Route.Home,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        composable<Route.Home> {
+            HomeScreen(
+                onNavigateToUpload = { imageUris ->
+                    navController.navigate(Route.Solving(imageUris))
+                },
+                onNavigateToSearch = {
+                    navController.navigate(Route.Search)
+                },
+            )
+        }
+
+        composable<Route.Search> {
+            SearchScreen(onNavigateBack = { navController.popBackStack() })
+        }
+
+        composable<Route.Solving> { backStackEntry ->
+            val route = backStackEntry.toRoute<Route.Solving>()
+            SolvingScreen(
+                imageUris = route.imageUris,
+                onNavigateBack = { navController.popBackStack() },
+            )
+        }
     }
-    composable("search") { SearchScreen(onNavigateBack = { navController.popBackStack() }) }
-    composable(
-        route = "solving/{imageUris}",
-        arguments = listOf(navArgument("imageUris") { type = NavType.StringType }),
-    ) { backStackEntry ->
-      val encodedUris = backStackEntry.arguments?.getString("imageUris") ?: ""
-      val imageUris = encodedUris.split(",").map { Uri.decode(it) }
-      SolvingScreen(
-          imageUris = imageUris,
-          onNavigateBack = { navController.popBackStack() },
-      )
-    }
-  }
 }
